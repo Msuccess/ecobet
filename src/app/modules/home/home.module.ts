23e1689b3b637279f9457056a5b047f1e16b46d6
Moses Nkrumah <mosessccss@gmail.com>
2021-07-26 08:08:24 +0000

    removed js

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+];
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  declarations: [HomeComponent],
+  imports: [CommonModule, HomeRoutingModule, RouterModule.forChild(routes)],
 })
-export class HomeModule { }
+export class HomeModule {}
